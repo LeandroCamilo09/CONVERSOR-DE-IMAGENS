bf5fa7bfb1576e875d50447ea254d821327cfa6c
Leandro Camilo <leandrocamilo09@gmail.com>
2025-03-19 21:45:31 -0300

    confirmação

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# from controllers.controllers import programa,arquivos,arquivos_salvos
 from controllers.controllers import programa, arquivos, Arquivos_salvos
 import flet as ft
 
@@ -8,14 +7,14 @@ def main(page: ft.Page):
      page.window.height=500
      page.theme_mode = "DARK"
      page.overlay.append(arquivos)
-     # page.window.center()
+     page.window.center()
      arquivo_salvo=Arquivos_salvos(aplicativo=page)
      page.overlay.append(arquivo_salvo)
      
 
      page.title = "Conversor de imagens_1.0"
-     p=programa(arq=arquivo_salvo)
+     app=programa(arq=arquivo_salvo)
      
-     page.add(p)
+     page.add(app)
 
 ft.app(target=main)
